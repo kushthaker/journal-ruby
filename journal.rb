01f00da8eecc5edcd2a5f52a37f17c4c74706be3
Kush <kushthaker@gmail.com>
2015-07-09 17:40:53 -0400

    set up journal hash with unique IDs

--- journal.rb
+++ journal.rb
@@ -1,12 +1,43 @@
 class Journal
-	attr_accessor :something
+	attr_accessor :entries
+	attr_accessor :used_entry_ids
 
 	def initialize 
-		@something = "some string";
+		@entries = {};
+		@used_entry_ids = []
 	end
 
+	def add_entry title, content, author
+		entry = {
+			title: "#{title}",
+			content: "#{content}",
+			author: "#{author}"
+		}
+
+		entry_id = generate_entry_id
+
+		@entries[entry_id] = entry
+	end
+
+
+	def generate_entry_id
+		entry_id =  (Random.rand * 1000).to_i
+
+		#the way to generate uuid is requiring 'securerandom' module
+		if (used_entry_ids.include? entry_id) == false
+			used_entry_ids.push(entry_id)
+		else
+			generate_entry_id
+		end
+
+		return entry_id
+	end
 end
 
-my_journal = Journal.new
+	journal = Journal.new
 
-puts my_journal.something
+	journal.add_entry "my title", "my content", "me"
+	journal.add_entry "my title 2", "my content 2", "me 2"
+
+	puts journal.entries
+
